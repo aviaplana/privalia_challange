06156f034b199eb0742e767200ab6a3c9e9ea2c1
Albert <albert@MacBook-MacBook-Pro-de-Albert.local>
2018-01-18 01:09:51 +0100

    Created Movie entities, as well as mappers.

---
+++ domain/src/main/java/com/privalia/albert/challange/domain/dto/MovieDto.java
@@ -0,0 +1,43 @@
+package com.privalia.albert.challange.domain.dto;
+
+import java.util.Date;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by albert on 18/1/18.
+ */
+
+@Setter
+@Getter
+public class MovieDto {
+    private int id;
+
+    private int voteCount;
+
+    private boolean video;
+
+    private String metadataType;
+
+    private String title;
+
+    private float popularity;
+
+    private String posterPath;
+
+    private String originalLanguage;
+
+    private String originalTitle;
+
+    private List<Integer> genereIds;
+
+    private String backdropPath;
+
+    private boolean adult;
+
+    private String overview;
+
+    private Date releaseDate;
+}
